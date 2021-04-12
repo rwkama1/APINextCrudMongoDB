7dd7b0b85c242d73765eb6bcb210a421870eb2fc
rwkama1 <rwkamandriw@gmail.com>
2021-04-11 22:18:46 -0300

    first

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+export default class Index extends Component
+{
+  render()
+  {
+    return(<div>
+      API REST NEXT CRUD MongoDB
+     </div>
+    )
+  }
+  
+}
+// export default function Index() {
+//   return (
+  
+//   )
+// }
